ee829f676cce9c0ac5462a2e100ee7082511eb33
Daniel Blank <dblank19@gmail.com>
2015-11-24 09:48:00 +0000

    corrected some keyign errors

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,12 @@
 class ContactMailer < ActionMailer::Base
     default to: 'dblank19@gmail.com'
     
-    def contact(name, email, body)
+    def contact_email(name, email, body)
        @name = name
        @email = email
        @body = body
        
-       mail(from: email, subject: 'Contact form message')
+       mail(from: @email, subject: 'Contact form message')
         
     end
     
